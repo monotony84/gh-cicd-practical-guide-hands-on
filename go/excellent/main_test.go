4c6fedba4581f73d0c0db30a9b6136387641a314
monotony84 <monotony84.d@gmail.com>
2024-10-01 22:35:27 +0900

    update

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func testEvenOrOdd(t *testing.T) {
 	result := EvenOrOdd(10)
-	if result != "even" {
-		t.Errorf("expected: even, actual: %s", result)
+	if result != "Even" {
+		t.Errorf("expected: Even, actual: %s", result)
 	}
 }
